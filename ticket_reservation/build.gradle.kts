8284ff1da932b316282e8397ddba6d9c24f6172e
BaekGeunYoung <dvmflstm@naver.com>
2020-06-01 01:14:36 +0900

    redisson dependency 추가

--- ticket_reservation/build.gradle.kts
+++ ticket_reservation/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.redisson:redisson:3.13.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
