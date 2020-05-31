170a46445b0b083dedf75e49e6c7190078ecf11c
BaekGeunYoung <dvmflstm@naver.com>
2020-06-01 01:15:03 +0900

    상수를 담는 Constants 클래스 작성

---
+++ ticket_reservation/src/main/kotlin/com/geunyoung/ticket_reservation/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.geunyoung.ticket_reservation.util
+
+class Constants {
+    companion object {
+        const val MAX_RESERVATION_COUNT = 50000
+        const val RESERVATION_LOCK_NAME = "reservationLock"
+    }
+}
