c5bca89279fa487ca4ede37c4d5dd83215aa05c1
BaekGeunYoung <dvmflstm@gmail.com>
2020-06-07 22:46:43 +0900

    불필요한 주석 제거

--- ticket_reservation/src/main/kotlin/com/geunyoung/ticket_reservation/service/impl/ReservationServiceImpl.kt
+++ ticket_reservation/src/main/kotlin/com/geunyoung/ticket_reservation/service/impl/ReservationServiceImpl.kt
@@ -9,7 +9,6 @@ import com.geunyoung.ticket_reservation.util.Constants
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
-import java.util.concurrent.TimeUnit
 
 @Service
 class ReservationServiceImpl(
@@ -17,11 +16,10 @@ class ReservationServiceImpl(
         @Autowired private val redissonService: RedissonService
 ): ReservationService {
     override fun reserve(reserveDtoMono: Mono<ReserveDto>, userIdMono: Mono<Long>): Mono<Boolean> {
-//        val lock = redissonService.getLock(Constants.RESERVATION_LOCK_NAME)
-
         val countMono = redissonService.getAtomicLong("count")
-        return countMono.get().flatMap {count ->
-            reserveDtoMono.flatMap { reserveDto ->
+        return countMono.get().flatMap { count ->
+            if (count >= Constants.MAX_RESERVATION_COUNT) Mono.just(false)
+            else reserveDtoMono.flatMap { reserveDto ->
                 userIdMono.flatMap { userId ->
                     reservationRepository.save(Reservation(userId = userId, number = reserveDto.number)).flatMap {
                         countMono.getAndSet(count + reserveDto.number).flatMap {
@@ -31,25 +29,5 @@ class ReservationServiceImpl(
                 }
             }
         }
-
-//        return lock.tryLock(10000, 5000, TimeUnit.MILLISECONDS).flatMap {
-//            if (it) {
-//                val countMono = redissonService.getAtomicLong("count")
-//
-//                return@flatMap countMono.get().flatMap { count ->
-//                    if (count >= Constants.MAX_RESERVATION_COUNT) Mono.just(false)
-//                    else reserveDtoMono.flatMap { reserveDto ->
-//                            userIdMono.flatMap { userId ->
-//                                reservationRepository.save(Reservation(userId = userId, number = reserveDto.number)).flatMap {
-//                                    countMono.getAndSet(count + reserveDto.number).flatMap {
-//                                        Mono.just(true)
-//                                    }
-//                                }
-//                            }
-//                        }
-//                }
-//            }
-//            else Mono.just(false)
-//        }
     }
 }
