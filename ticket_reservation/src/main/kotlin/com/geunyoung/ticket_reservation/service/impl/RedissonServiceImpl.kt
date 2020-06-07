e9e726fe3643bf31c63365318d42a9ed975e0359
BaekGeunYoung <dvmflstm@gmail.com>
2020-06-07 15:47:05 +0900

    redissonClient 생성을 init 에서 하지 않고 @PostConstruct 에서 하도록 설정

--- ticket_reservation/src/main/kotlin/com/geunyoung/ticket_reservation/service/impl/RedissonServiceImpl.kt
+++ ticket_reservation/src/main/kotlin/com/geunyoung/ticket_reservation/service/impl/RedissonServiceImpl.kt
@@ -7,18 +7,19 @@ import org.redisson.api.RLockReactive
 import org.redisson.api.RedissonReactiveClient
 import org.redisson.config.Config
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
+import javax.annotation.PostConstruct
 
 @Service
 class RedissonServiceImpl : RedissonService {
-    @Value("\${REDIS_HOST:localhost}")
-    private var redisHost: String = ""
+    @Value("\${REDIS_HOST:127.0.0.1}")
+    private lateinit var redisHost: String
     private val config = Config()
     private lateinit var redissonClient: RedissonReactiveClient
     private lateinit var redisUri: String
 
-    init {
+    @PostConstruct
+    fun initialize() {
         redisUri = "redis://$redisHost:6379"
         config.useSingleServer().address = redisUri
         redissonClient = Redisson.createReactive(config)
